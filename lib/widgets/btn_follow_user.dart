53306289d9df1a6b3b4afff9b501c2eacbe86e1b
Xavier Zabala M <xzabalam@gmail.com>
2022-10-04 10:59:23 -0500

    Rutas

---
+++ lib/widgets/btn_follow_user.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_maps_app/blocs/blocs.dart';
+
+class BtnFollowUser extends StatelessWidget {
+  const BtnFollowUser({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mapBloc = BlocProvider.of<MapBloc>(context);
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        maxRadius: 25,
+        child: BlocBuilder<MapBloc, MapState>(
+          builder: (context, state) {
+            return IconButton(
+              icon: Icon(
+                  state.isFollowingUser
+                      ? Icons.directions_run_rounded
+                      : Icons.hail_rounded,
+                  color: Colors.black),
+              onPressed: () {
+                mapBloc.add(OnStartFollowingUserMapEvent());
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/btn_toggle_user_route.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_maps_app/blocs/blocs.dart';
+
+class BtnToggleUserRoute extends StatelessWidget {
+  const BtnToggleUserRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mapBloc = BlocProvider.of<MapBloc>(context);
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        maxRadius: 25,
+        child: IconButton(
+          icon: Icon(Icons.more_horiz_rounded, color: Colors.black),
+          onPressed: () {
+            mapBloc.add(OnToggleUserShowRouteEvent());
+          },
+        ),
+      ),
+    );
+  }
+}
